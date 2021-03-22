03222bd2d5c1cf5f2a74429fa62c0e427597a79b
kebi3477 <kebi3477@naver.com>
2021-03-23 00:14:20 +0900

    invite 추가

--- js/pay.js
+++ js/pay.js
@@ -1,12 +1,20 @@
 const canvas = document.querySelector(".pay--canvas");
 initCanvas(canvas);
+const payButton = document.querySelector(".pay_button");
+const button = document.querySelector(".after_button");
 let cnt = 0;
+
 canvas.addEventListener("touchmove", function(e) {
     cnt++;
 })
 canvas.addEventListener("touchend", function() {
     if(cnt > 250) {
-        alert("계산 성공!");
+        // alert("계산 성공!");
+        button.classList.add("after_button_active");
+        button.onclick = () => location.href = 'invite.html';
         cnt = 0;
     }
+})
+payButton.addEventListener("click", function() {
+    this.parentElement.classList.remove("popup_second");
 })
